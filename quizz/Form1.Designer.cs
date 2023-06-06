3b5f0821eaa646746d533e9ab6c8f2b4f9ef7bbf
SaintRase <116195719+SaintRase@users.noreply.github.com>
2023-06-06 08:40:13 +0200

    Add project files.

---
+++ quizz/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿using System.Drawing;
+
+namespace quizz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.operand1Label = new System.Windows.Forms.Label();
+            this.operatorLabel = new System.Windows.Forms.Label();
+            this.operand2Label = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.validateButton = new System.Windows.Forms.Button();
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arithmetic Drills";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(146, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Problem 1 / 10";
+            // 
+            // operand1Label
+            // 
+            this.operand1Label.AutoSize = true;
+            this.operand1Label.Location = new System.Drawing.Point(3, 0);
+            this.operand1Label.Name = "operand1Label";
+            this.operand1Label.Size = new System.Drawing.Size(21, 23);
+            this.operand1Label.TabIndex = 2;
+            this.operand1Label.Text = "label3";
+            // 
+            // operatorLabel
+            // 
+            this.operatorLabel.AutoSize = true;
+            this.operatorLabel.Location = new System.Drawing.Point(32, 0);
+            this.operatorLabel.Name = "operatorLabel";
+            this.operatorLabel.Size = new System.Drawing.Size(21, 23);
+            this.operatorLabel.TabIndex = 3;
+            this.operatorLabel.Text = "label4";
+            // 
+            // operand2Label
+            // 
+            this.operand2Label.AutoSize = true;
+            this.operand2Label.Location = new System.Drawing.Point(61, 0);
+            this.operand2Label.Name = "operand2Label";
+            this.operand2Label.Size = new System.Drawing.Size(21, 23);
+            this.operand2Label.TabIndex = 4;
+            this.operand2Label.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "=";
+            // 
+            // validateButton
+            // 
+            this.validateButton.BackColor = System.Drawing.SystemColors.Control;
+            this.validateButton.Location = new System.Drawing.Point(169, 337);
+            this.validateButton.Name = "validateButton";
+            this.validateButton.Size = new System.Drawing.Size(75, 23);
+            this.validateButton.TabIndex = 6;
+            this.validateButton.Text = "Submit";
+            this.validateButton.UseVisualStyleBackColor = false;
+            this.validateButton.Click += new System.EventHandler(this.validateButton_Click);
+            // 
+            // answerTextBox
+            // 
+            this.answerTextBox.Location = new System.Drawing.Point(157, 296);
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.answerTextBox.TabIndex = 7;
+            this.answerTextBox.TextChanged += new System.EventHandler(this.CheckAnswer);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.00062F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.00063F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.00063F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.99813F));
+            this.tableLayoutPanel1.Controls.Add(this.operand1Label, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.operatorLabel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.operand2Label, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(151, 258);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(118, 23);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // restartButton
+            // 
+            this.restartButton.BackColor = System.Drawing.Color.Yellow;
+            this.restartButton.Location = new System.Drawing.Point(129, 379);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(75, 23);
+            this.restartButton.TabIndex = 9;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Red;
+            this.exitButton.Location = new System.Drawing.Point(215, 378);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::quizz.Properties.Resources._3bQD1J_star_clipart_hd_photo_png;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 207);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(78, 74);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::quizz.Properties.Resources._51_518459_math_clip_art_for_middle_school_free_clipart_images_math_clipart_png_transformed;
+            this.pictureBox2.Location = new System.Drawing.Point(324, 164);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(93, 155);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::quizz.Properties.Resources._704904_transformed;
+            this.pictureBox1.Location = new System.Drawing.Point(141, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.answerTextBox);
+            this.Controls.Add(this.validateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Arithmetic Quiz";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label operand1Label;
+        private System.Windows.Forms.Label operatorLabel;
+        private System.Windows.Forms.Label operand2Label;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button validateButton;
+        private System.Windows.Forms.TextBox answerTextBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ quizz/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizz
+{
+    public partial class Form1 : Form
+    {
+        // declaring my variables
+        private readonly Random random = new Random();
+        private int operand1;
+        private int operand2;
+        private char operatorChar;
+        private int score;
+        private int currentProblem;
+        private const int TotalProblems = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateProblem();
+        }
+
+        
+        private void GenerateProblem()
+        {
+            // gets a random operator
+            operatorChar = GetRandomOperator();
+
+            if (operatorChar == '×')
+            {
+                operand1 = GetRandomNumber(1, 12);
+                operand2 = GetRandomNumber(1, 12);
+            }
+            else if (operatorChar == '÷')
+            {
+                operand2 = GetRandomNumber(1, 12);
+                operand1 = operand2 * GetRandomNumber(1, 12);
+            }
+            else
+            {
+                operand1 = GetRandomNumber(1, 50);
+                operand2 = GetRandomNumber(1, 50);
+            }
+
+            //here the labels will update to actually show the current problem
+            operand1Label.Text = operand1.ToString();
+            operand2Label.Text = operand2.ToString();
+            operatorLabel.Text = operatorChar.ToString();
+            answerTextBox.Text = "";
+
+            label2.Text = $"Problem {currentProblem} / {TotalProblems}";
+        }
+
+        private char GetRandomOperator()
+        {
+            string operators = "+-×÷";
+            int randomIndex = random.Next(operators.Length);
+            return operators[randomIndex];
+        }
+
+        private int GetRandomNumber(int min, int max)
+        {
+            return random.Next(min, max + 1);
+        }
+
+        //this button checks the answer provided and gives feedback based on it
+        private void validateButton_Click(object sender, EventArgs e)
+        {
+            int correctAnswer = CalculateCorrectAnswer();
+
+            //check if answer provided is actually an int and equal to the already calculated correct answer
+            if (int.TryParse(answerTextBox.Text, out int userAnswer) && userAnswer == correctAnswer)
+            {
+                score++;
+                MessageBox.Show("Correct!");
+            }
+            else
+            {
+                MessageBox.Show($"Wrong! The correct answer is {correctAnswer}");
+            }
+
+            currentProblem++;
+            if (currentProblem <= TotalProblems)
+            {
+                GenerateProblem();
+            }
+            else
+            {
+                MessageBox.Show($"Quiz Completed! Final Score: {score}/{TotalProblems}");
+                RestartQuiz();
+            }
+        }
+
+        private int CalculateCorrectAnswer()
+        {
+            switch (operatorChar)
+            {
+                case '+':
+                    return operand1 + operand2;
+                case '-':
+                    return operand1 - operand2;
+                case '×':
+                    return operand1 * operand2;
+                case '÷':
+                    return operand1 / operand2;
+                default:
+                    throw new ArgumentException("Invalid operator");
+            }
+        }
+
+        private void RestartQuiz()
+        {
+            var result = MessageBox.Show("Do you want to restart the quiz?", "Quiz Completed", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                score = 0;
+                currentProblem = 1;
+                GenerateProblem();
+            }
+            else if (result == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            RestartQuiz();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //this function checks the answer input as its being typed to prevent anything other than numbers from being typed
+        private void CheckAnswer(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(answerTextBox.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter a number!");
+                answerTextBox.Text = answerTextBox.Text.Remove(answerTextBox.Text.Length - 1);
+            }
+        }
+    }
+}
